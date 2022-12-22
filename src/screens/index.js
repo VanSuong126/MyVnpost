eba212fa381b969da9dd746c927ad712cd6a0402
Van Suong <tvsuongit@gmail.com>
2022-12-22 23:16:06 +0700

    update link

--- src/screens/index.js
+++ src/screens/index.js
@@ -2,7 +2,7 @@
 import Login from './Auths/Login';
 import Home from './Main/Home/home';
 import Dashboard from './Main/Dashboard/dashboard';
-import CreateOrder from './Main/Dashboard/CreateOrder/createOrder';
+import CreateOrder from './Main/Dashboard/CreateOrder';
 
 export {
     Login,
